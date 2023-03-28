86084a12adf70483bbd74a2b8ff21ba392363870
thomas-hudson-dcms <123599292+thomas-hudson-dcms@users.noreply.github.com>
2023-03-28 13:39:15 +0100

    v0.2.0

--- checks_module.R
+++ checks_module.R
@@ -1,35 +1,89 @@
+required_columns <-
+  as.vector(read.csv(
+    "required_columns.csv",
+    header = FALSE,
+    colClasses = "character"
+  )$V1)
+
 # Define the UI for a module
-checksUI <- function(id, label = "Checks") {
+checks_ui <- function(id, label = "checks_ui") {
   ns <- NS(id)
-  sidebarLayout(sidebarPanel(
-    selectInput(
-      inputId = "selected_check",
+  sidebarLayout(
+    sidebarPanel(selectInput(
+      inputId = ns("selected_check"),
       label = "",
       choices = c(
-        "All",
-        "1. Column names",
-        "2. Duplicate UPRNs",
-        "3. Current Technology"
+        '1. Column names' = "selected_check_1",
+        '2. Duplicate UPRNs' = "selected_check_2",
+        '3. Current Technology' = "selected_check_3"
       )
+    ),
+    conditionalPanel(
+      condition = "input.selected_check == 'selected_check_2'",
+      selectInput(ns("column"), label = "Select a column:", choices = NULL)
     )
-  ),
-  mainPanel(
-    verbatimTextOutput("upload_status")
-  ))
+    ),
+    mainPanel(
+      verbatimTextOutput(ns("upload_status")),
+      verbatimTextOutput(ns("check_1")),
+      verbatimTextOutput(ns("check_2"))
+    )
+  )
 }
 
 # Define the server logic for a module
-checksServer <- function(id) {
-  moduleServer(
-    id,
-    function(input, output, session) {
-      
-      output$upload_status <- renderPrint({
-        if (is.null(input$file_upload)) {
-          cat("No file uploaded.")
-        }
-      })
-      
-    }
-  )
+checks_server <- function(id, data, file_upload) {
+  moduleServer(id,
+               function(input, output, session) {
+                 
+                 observeEvent(file_upload(), {
+                   updateSelectInput(session, "column", choices = colnames(data()))
+                 })
+                 
+                 
+                 output$upload_status <- renderPrint({
+                   if (is.null(file_upload())) {
+                     cat("No file uploaded.")
+                   }
+                 })
+                 
+                 output$check_1 <- renderPrint({
+                   if (!is.null(file_upload()) &
+                       input$selected_check == "selected_check_1") {
+                     # Check if all the required columns exist in the data table
+                     missing_columns <-
+                       setdiff(tolower(required_columns), tolower(colnames(data())))
+                     
+                     # Output the result
+                     if (length(missing_columns) == 0) {
+                       cat("PASSED check 1. \nAll required columns are present.\n")
+                     } else {
+                       cat("FAILED check 1. \nThe following columns are missing:\n")
+                       cat(paste0("-", missing_columns, "\n"))
+                     }
+                   }
+                 })
+                 
+                 
+                 output$check_2 <- renderPrint({
+                   if (!is.null(file_upload()) & input$selected_check == "selected_check_2") {
+                       req(input$column)
+                       uprns <- data()[, input$column, drop = FALSE]
+                       duplicate_count = sum(duplicated(na.omit(uprns)))
+                       
+                       # Output the result
+                       if (duplicate_count == 0) {
+                         cat("PASSED check 2. \nNo duplicate UPRNs are present.\n")
+                       } else {
+                         cat(
+                           "FAILED check 2. \nThere are:",
+                           format(duplicate_count, big.mark = ","),
+                           "duplicates within the '", input$column, "' column of the uploaded data.\n"
+                         )
+                       }
+                       
+                     }
+                 })
+               })
+  
 }
